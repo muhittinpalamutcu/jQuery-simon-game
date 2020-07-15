11c592d5c7a0f9f8c2cdf3545f6cd3d677f340bd
muhittinpalamutcu <muhittinpalamutcu@gmail.com>
2020-07-15 19:07:26 +0300

    Add click event and some styles

--- game.js
+++ game.js
@@ -1,1 +1,25 @@
+var gamePath = [];
+var userClickPath = [];
+var buttonColors = ["green", "red", "yellow", "blue"];
 
+function nextSequence() {
+  var randomNumber = Math.random();
+  randomNumber = randomNumber * 4;
+  randomNumber = Math.floor(randomNumber);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePath.push(randomChosenColor);
+}
+
+$(".btn").on("click", function(event) {
+  //console.log(event.target.id);
+  var userChosenColor = event.target.id;
+  $("#" + userChosenColor).fadeOut(100).fadeIn(100);
+  playSound(userChosenColor);
+  userClickPath.push(userChosenColor);
+  console.log(userClickPath);
+});
+
+function playSound(color) {
+  var audio = new Audio('sounds/' + color + '.mp3');
+  audio.play();
+}
